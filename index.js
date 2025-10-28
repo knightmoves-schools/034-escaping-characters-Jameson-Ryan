ec2507081a34cb1a5a788f6f8ecaab86560807f4
Jameson-Ryan <jamrya0726@students.maquoketaschools.org>
2025-10-28 14:11:59 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+let introduction = '\tHello, I am Jameson and I \'totally\' know how to code\n\n' +
+  'Also, I did not say \"I know how to code\", that must have been AI \\o/'
+
+document.getElementById("result").innerHTML = introduction;
